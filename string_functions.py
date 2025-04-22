77cd89c8fd0826624cf000067559aab6f3d27809
Shivacharan <shivacharanofficial2004@gmail.com>
2025-04-22 22:56:11 +0530

    Initial commit

---
+++ string_functions.py
@@ -0,0 +1,12 @@
+#STRING FUNCTIONS#
+
+
+a="shivacharan"
+print(len(a)) # to find length of a string
+print(a.capitalize())# turns first letter to capital
+print(a.upper()) #converts string to uppercase
+print(a.lower()) #converts string to lowercase
+print(a.replace("shiva","ram")) #converts the existing string to a new string
+print(a.startswith("sh")) #if the string is starting with the given parameters returns true else false
+print(a.endswith("and"))
+print(a.find("charan")) #returns the starting location of the substring
