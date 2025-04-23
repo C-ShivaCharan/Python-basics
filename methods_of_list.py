e9eaed6937fc880bcd23a75f8bdc7b5d119ed3fd
Shivacharan <shivacharanofficial2004@gmail.com>
2025-04-23 11:28:55 +0530

    'commit'

---
+++ methods_of_list.py
@@ -0,0 +1,20 @@
+                      # METHODS OF LIST
+a=["shiva",21,2.03,False,'m']
+print(a)
+a.append("akul") #append method is used to insert any value at the end of the end of list
+print(a)
+list1=[24,13,2,1,5]
+print(list1)
+list1.sort() #sort method is used to sort the given list
+print(list1)
+list1.reverse()
+print(list1) #reverse is used to print the list in reverse order
+# insert method is used to insert any element at particular index syntax:str.insert(index,value)
+a.insert(2,"python")
+print(a)
+# pop method is used to remove any element from particular index
+a.pop(3)
+print(a.pop(3))
+print(a)
+a.remove("shiva") # using remove method we can remove any element from the list syntax:str.remove(value)
+print(a)
---
+++ slicing_skipping.py
@@ -0,0 +1,3 @@
+a="0123456789"
+sub=a[1:9:2] #the last number after colen tells how many times a string should skip the printing
+print(sub) 
