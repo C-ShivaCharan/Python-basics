d839c2dee210fff9dd490daeb6b4cb080d523543
Shivacharan <shivacharanofficial2004@gmail.com>
2025-04-24 10:43:34 +0530

    "commit"

---
+++ area and circumference of circle.py
@@ -0,0 +1,6 @@
+import math
+r=float(input("enter the radius of the circle:"))
+area=math.pi*r*r
+circumference=2*math.pi*r
+print(f"Area of circle is {round(area,2)} sqr units.")
+print(f"Circumference of circle is{round(circumference,2)} units")
